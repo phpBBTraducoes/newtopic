5b6e5e0c0c48b05236721744158a50f8613309b3
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-11-07 21:44:29 -0200

    Traducao ADD

---
+++ language/pt_br/info_acp_newtopic.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* New Topic [Brazilian Portuguese [pt_br]] 
+* Brazilian Portuguese  translation by eunaumtenhoid [ver 1.0.1] (https://github.com/phpBBTraducoes)
+* @package info_acp_newtopic.php
+* @copyright BB3.MOBi (c) 2015 Anvar http://apwa.ru
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_NEWTOPIC'					=> 'Criar tópico rapidamente ',
+	'ACP_NEWTOPIC_FORUM'			=> 'Seleção de fórum',
+	'ACP_NEWTOPIC_FORUM_EXPLAIN'	=> 'Selecione o fórum que será exibido na lista suspensa quando você criar um novo tópico..<br />Selecione vários fóruns segurando <samp>CTRL</samp> e clicando.',
+	'ACP_NEWTOPIC_BUTTON'			=> 'Nome do botão, substituição de "Novo tópico"',
+));
